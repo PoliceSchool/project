54afcd3bce5cb1987e87f74019905127d801371b
policeschool <policeschool@126.com>
2020-02-29 20:05:35 +0800

    重建项目

--- springboot_atguigu/spring-boot-01-hellowworld/src/main/java/com/atguigu/HelloWorldApplication.java
+++ springboot_atguigu/spring-boot-01-hellowworld/src/main/java/com/atguigu/HelloWorldApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * @SpringBootApplication 来标注一个主程序类, 说明这是一个SpringBoot应用
+ * 这个@SpringBootApplication 来标注一个主程序类, 说明这是一个SpringBoot应用
  */
 @SpringBootApplication
 public class HelloWorldApplication {
