8bf4b7c4b64bf0a9b542fff272b20bb196c80dd1
policeschool <policeschool@126.com>
2020-03-04 21:58:47 +0800

    集成spring-cloud

---
+++ springboot_atguigu/springboot-springcloud/eureka-server/src/main/java/com/atguigu/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,19 @@
+package com.atguigu.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 1. 配置eureka信息
+ * 2. @EnableEurekaServer
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
