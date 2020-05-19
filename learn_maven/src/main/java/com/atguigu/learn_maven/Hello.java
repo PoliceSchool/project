03f094075f251e64647633448b0fe84e15f14f60
policeschool <policeschool@126.com>
2020-05-19 19:48:52 +0800

    first commit

---
+++ learn_maven/src/main/java/com/atguigu/learn_maven/Hello.java
@@ -0,0 +1,7 @@
+package com.atguigu.learn_maven;
+
+public class Hello {
+    public String sayHello(String name){
+        return "Hello " + name + "!";
+    }
+}
---
+++ learn_maven/src/test/java/com/atguigu/learn_maven/HelloTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.learn_maven;
+
+import org.junit.Test;
+import static junit.framework.Assert.*;
+
+public class HelloTest {
+    @Test
+    public void testHello(){
+	Hello hello = new Hello();
+	String results = hello.sayHello("litingwei");
+	assertEquals("Hello litingwei!", results);
+    }
+}
